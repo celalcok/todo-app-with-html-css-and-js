b55c3a62a6823d20b2cacafdff8f3ed5573d8e2d
celalcok <celalcok@gmail.com>
2022-08-14 17:49:54 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Todo App with HTML, CSS and Javascript
+![todo-app](todo.gif "Todo App with HTML, CSS and Javascript")
+
+This application is developed using HTML5, CSS and Javascript.
---
+++ script.js
@@ -0,0 +1,71 @@
+
+// defining variables
+let addTodoInput = document.getElementById("addTodoInput");
+let addTodoButton = document.getElementById("addTodoButton");
+let todosContainer = document.getElementById("todosContainer");
+let message = document.getElementById("message");
+
+
+
+if(todosContainer.childNodes.length <2){
+    todosContainer.style.visibility="hidden";
+}
+else{
+    todosContainer.style.visibility="visible";
+}
+
+
+function addTodo(){
+    
+    if(addTodoInput.value.length < 3){
+        message.innerHTML = "Please enter some text";
+    }
+    else{
+
+
+        if(todosContainer.childNodes.length < 1){
+            todosContainer.style.visibility = "hidden";
+        }
+        else{
+            todosContainer.style.visibility = "visible";
+        }
+        
+        message.innerHTML = "";
+        
+
+        // defining todo item
+        var todoItem = document.createElement("div");
+        todoItem.className ="todo-item";
+        
+        // defining todo text
+        var todoText  = document.createElement("p");
+        todoText.className = "todo-text";
+        todoText.innerText = addTodoInput.value;
+        
+        // defining todo remove button
+        var todoRemoveButton = document.createElement("button");
+        todoRemoveButton.className ="btn-remove";
+        todoRemoveButton.innerText="Remove";
+        
+        // eventlistener for remove button
+        todoRemoveButton.addEventListener("click", (e) =>{
+            todoRemoveButton.parentElement.remove();
+            if(todosContainer.childNodes.length <2){
+                todosContainer.style.visibility="hidden";
+            }
+            else{
+                todosContainer.style.visibility="visible";
+            }
+        })
+
+        // Add children to parent element
+        todoItem.appendChild(todoText);
+        todoItem.appendChild(todoRemoveButton);
+        todosContainer.appendChild(todoItem);
+
+        addTodoInput.value="";
+    }
+}
+
+
+addTodoButton.addEventListener("click",addTodo);
